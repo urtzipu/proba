ae741608910472732037f182427bf06f6b0c3f6d
juanan <juanan.pereira@ehu.es>
2012-09-27 12:58:19 +0200

    Lehenengo bertsioa (Hauteskundeetan mahaiak kudeatzeko aplikazioa)

---
+++ hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
@@ -0,0 +1,27 @@
+package com.isad.bertsioak;
+
+import java.util.ArrayList;
+
+public class Hauteskundeak {
+
+	private ArrayList<String> mahaiak = new ArrayList<String>();
+
+	@Override
+	public String toString() {
+		String emaitza = "";
+		for (String mahai : mahaiak) {
+			emaitza += mahai + " ";
+		}
+		return emaitza;
+	}
+	private void gehituMahaia(String mahai) {
+			mahaiak .add(mahai);
+	}
+
+	public static void main(String[] args) {
+		Hauteskundeak h2012 = new Hauteskundeak();
+		h2012.gehituMahaia("Irun");
+		System.out.println(h2012);
+	}
+
+}
