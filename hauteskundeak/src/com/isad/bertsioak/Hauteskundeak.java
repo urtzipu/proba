579e263d1c02a5bafc98265dee6ebd02d20bb889
Juanan Pereira <juanan.pereira@ehu.eus>
2018-09-16 21:55:33 +0200

    Andoian adar berrian sartu

--- hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
+++ hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
@@ -25,6 +25,7 @@ public class Hauteskundeak {
 		h2012.gehituMahaia("Donostia");
 		h2012.gehituMahaia("Tolosa");
 		h2012.gehituMahaia("Bergara");
+		h2012.gehituMahaia("Andoain");
 		
 		System.out.println(h2012);
 	}
